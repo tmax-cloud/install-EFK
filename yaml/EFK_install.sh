5b5e04563a581f2ab61e18c95bb94b13642bed3c
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-02-15 17:33:27 +0900

    [mod] CR, SC 파라미터 삭제/변경

--- yaml/EFK_install.sh
+++ yaml/EFK_install.sh
@@ -4,19 +4,19 @@ export ES_VERSION=7.2.0
 export KIBANA_VERSION=7.2.0
 export FLUENTD_VERSION=v1.4.2-debian-elasticsearch-1.1
 export BUSYBOX_VERSION=1.32.0
-if [ -z $2 ]; then
-  export STORAGECLASS_NAME=csi-cephfs-sc
+if [ -z $1 ]; then
+  export STORAGECLASS_NAME=
+  echo "STORAGECLASS_NAME = Default-StorageClass"
 else
-  export STORAGECLASS_NAME=$2
+  export STORAGECLASS_NAME=$1
+  echo "STORAGECLASS_NAME = $STORAGECLASS_NAME"
 fi
-
 
 echo "ES_VERSION = $ES_VERSION"
 echo "KIBANA_VERSION = $KIBANA_VERSION"
 echo "FLUENTD_VERSION = $FLUENTD_VERSION"
 echo "BUSYBOX_VERSION = $BUSYBOX_VERSION"
-echo "STORAGECLASS_NAME = $STORAGECLASS_NAME"
-echo "ContainerRuntime = $1"
+#echo "ContainerRuntime = cri-o"
 
 sed -i 's/{busybox_version}/'${BUSYBOX_VERSION}'/g' 01_elasticsearch.yaml
 sed -i 's/{es_version}/'${ES_VERSION}'/g' 01_elasticsearch.yaml
@@ -31,12 +31,5 @@ kubectl create ns kube-logging
 
 kubectl apply -f 01_elasticsearch.yaml
 kubectl apply -f 02_kibana.yaml
-if [ -z $1 ] || [ "docker" == $1 ]; then
-  kubectl apply -f 03_fluentd.yaml
-elif [ "crio" == $1 ] || [ "cri-o" == $1 ]; then 
-  kubectl apply -f 03_fluentd_cri-o.yaml  
-else
-  echo "Unknown Container Runtime Error"
-fi
-
+kubectl apply -f 03_fluentd_cri-o.yaml  
 echo "---Installation Done---"
