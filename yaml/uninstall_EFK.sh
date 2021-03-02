d4042d18b28f33af2952e5172ba07dcf6faddd68
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-03-02 03:02:17 -0500

    [mod] uninstall 오류 수정

--- yaml/uninstall_EFK.sh
+++ yaml/uninstall_EFK.sh
@@ -1,33 +1,29 @@
 echo "---Uninstallation Start---"
-kubectl delete -f 03_fluentd_cri-o.yaml
-timeout 5m kubectl -n kube-logging wait daemonset/fluentd --for=delete
+timeout 5m kubectl delete -f 03_fluentd_cri-o.yaml 
 suc=`echo $?`
 if [ $suc != 0 ]; then
   echo "Failed to delete Fluentd"
-  exit 1
+  #exit 1
 fi
 
-kubectl delete -f 02_kibana.yaml
-timeout 5m kubectl -n kube-logging wait deployment/kibana --for=delete
+timeout 5m kubectl delete -f 02_kibana.yaml 
 suc=`echo $?`
 if [ $suc != 0 ]; then
   echo "Failed to delete Kibana"
-  exit 1
+  #exit 1
 fi
 
-kubectl delete -f 01_elasticsearch.yaml
-timeout 5m kubectl -n kube-logging wait statefulset/es-cluster --for=delete
+timeout 5m kubectl delete -f 01_elasticsearch.yaml
 suc=`echo $?`
 if [ $suc != 0 ]; then
   echo "Failed to delete ElasticSearch"
-  exit 1
+  #exit 1
 fi
 
-kubectl delete namespace kube-logging
-timeout 5m kubectl -n kube-logging wait namespace/kube-logging --for=delete
+timeout 5m kubectl delete namespace kube-logging
 suc=`echo $?`
 if [ $suc != 0 ]; then
   echo "Failed to delete namespace"
-  exit 1
+  #exit 1
 fi
 echo "---Uninstallation Done---"
